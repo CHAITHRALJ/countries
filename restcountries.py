5b5a9fd245b3e6f9f2f2c5073bc8d16971e10bd0
CHAITHRALJ <34154233+CHAITHRALJ@users.noreply.github.com>
2018-08-07 12:59:36 +0530

    Update restcountries.py

--- restcountries.py
+++ restcountries.py
@@ -6,9 +6,9 @@ def getData():  # fuction to get the data
   r = requests.get(url).text  # convert the json data to text format
   data = json.loads(r)  # loads the data to a variable
   for country in data:   # for all the countries in data
-    for tld in country['topLevelDomain']: # prints all the toplevel domain of the country
-      print(country['name'], end='')
-      print (',',tld)
+    for tld in country['topLevelDomain']: # for all toplevel domain of the country
+      print(country['name'], end='') # prints the country name 
+      print (',',tld)  # prints the all TLD's of the country
     print("\n")
 getData()   # returning the required output
 
